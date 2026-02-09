05b869e9c921b0805ad1e428e2002db5949c88dc
Daniel <d.vergara112@uniandes.edu.co>
2026-02-09 07:44:50 -0500

    database

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "arquisoft",
+        "USER": "admin",
+        "PASSWORD": "admin",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
